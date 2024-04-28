4f763897e3a5df8aa48ed681b1c4333230d54050
Deep Sanghani <deepsanghani3@gmil.com>
2024-04-28 18:56:03 +0530

    initial commit

---
+++ src/main/java/com/project/generatedynamicpdf/GenerateDynamicPdfApplication.java
@@ -0,0 +1,13 @@
+package com.project.generatedynamicpdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenerateDynamicPdfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenerateDynamicPdfApplication.class, args);
+	}
+
+}
